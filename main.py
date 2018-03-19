da419559bcf1e572fa4b74da0e96fadcd52cd0ac
Yu-k-i <1240342718@qq.com>
2018-03-19 12:07:07 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from werobot import WeRoBot
+from werobot.config import Config
+
+config = Config(
+    TOKEN='tangxin',
+    SERVER="auto",
+    HOST="0.0.0.0",
+    PORT="80",
+    SESSION_STORAGE=None,
+    APP_ID='wx98e9f23f0ba108c6',
+    APP_SECRET=None,
+    ENCODING_AES_KEY='36HbTSXwX3yU0NAbkvII2a0d6DLMK7TCm8e0bWQpZ3h '
+)
+
+robot = WeRoBot(config=config)
+
+@robot.handler
+def hello_world(message):
+    return 'hello world'
+
+@robot.text
+def test(message):
+    return message.content
+
+robot.run()
